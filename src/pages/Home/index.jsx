9ea4f6551d730b0b4d61074d6201f8f40920a528
rafcavalheiro <rafcavalheiro@gmail.com>
2022-06-08 17:22:49 -0300

    Atualizando estado com UseState

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,15 +1,20 @@
 import './styles.css'
+import React, { useState } from 'react'
 
 import { Card } from '../../components/Card'
 
- export function Home() {   
+ export function Home() {  
+   
+  const [studentName, setStudentName] = useState()
 
   return (
     
     <div className="container">
 
-      <h1>Lista de presença</h1>
-      <input type="text" placeholder="Digite o nome..."/>
+      <h1>Nome: {studentName}</h1>
+      <input type="text" 
+      placeholder="Digite o nome..."
+      onChange={ e => setStudentName(e.target.value)}/>
       <button type="button"> Adicionar </button>
 
       <Card name = "Rafael Cavalheiro" time="16:41:25"/>
